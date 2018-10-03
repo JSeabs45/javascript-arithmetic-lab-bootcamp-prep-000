d685036c8954629906b5867d57f1c6d5a62d44d3
Joshua Seabolt <jseabolt45@hotmail.com>
2018-10-03 15:12:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,3 +28,12 @@ function dec(a) {
   return number;
 }
 
+function makeInt(a) {
+  number = parseInt(a, 10);
+  return number;
+}
+
+function preserveDecimal(a) { 
+  number = parseFloat(a, 10);
+  return number;
+}
